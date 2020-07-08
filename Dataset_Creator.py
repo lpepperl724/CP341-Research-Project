f1000e529880c7404ab6ee4e8604b67b359c046f
mac <ha_wang@coloradocollege.edu>
2020-07-08 15:07:05 -0600

    datasetcreator

--- Dataset_Creator.py
+++ Dataset_Creator.py
@@ -1,5 +1,6 @@
-import cv2
+import numpy
 import os
+from PIL import Image
 
 images = []
 ages = []
@@ -7,17 +8,14 @@ genders = []
 
 def load_and_resize_images_from_folder(folder):
     for filename in os.listdir(folder):
-        try:
-            img = cv2.imread(os.path.join(folder,filename))
-            img = cv2.resize(img, (100, 100))
-            images.append(img)
-            extract_gender_and_age(filename)
-
-        except:
-            print("i")
+        img = Image.open(os.path.join(folder,filename))
+        img = img.resize((100, 100))
+        imgarr = numpy.array(img)
+        images.append(imgarr)
+        extract_gender_and_age(filename)
+        img.close()
 
 def extract_gender_and_age(filename):
-
 
     file_name_separator = filename.split('_')
 
@@ -26,4 +24,4 @@ def extract_gender_and_age(filename):
     genders.append(file_name_separator[1])
 
 load_and_resize_images_from_folder("/Users/mac/Desktop/Clone/CP341-Research-Project/part1")
-print(len(ages) == len(genders) == len(images))
+print(images[0])
