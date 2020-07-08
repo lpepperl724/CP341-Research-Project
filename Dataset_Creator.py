2b5822590dfea62864d14495a743da9299652379
mac <ha_wang@coloradocollege.edu>
2020-07-08 17:11:29 -0600

    datasetcreator

--- Dataset_Creator.py
+++ Dataset_Creator.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import os
 from PIL import Image
 
@@ -10,7 +10,7 @@ def load_and_resize_images_from_folder(folder):
     for filename in os.listdir(folder):
         img = Image.open(os.path.join(folder,filename))
         img = img.resize((100, 100))
-        imgarr = numpy.array(img)
+        imgarr = np.array(img)
         images.append(imgarr)
         extract_gender_and_age(filename)
         img.close()
@@ -19,9 +19,8 @@ def extract_gender_and_age(filename):
 
     file_name_separator = filename.split('_')
 
-    ages.append(file_name_separator[0])
+    ages.append(int(file_name_separator[0]))
 
-    genders.append(file_name_separator[1])
+    genders.append(int(file_name_separator[1]))
 
 load_and_resize_images_from_folder("/Users/mac/Desktop/Clone/CP341-Research-Project/part1")
-print(images[0])
