0e5401309425fd642826dfccdc5f1c07460d7d2c
mac <ha_wang@coloradocollege.edu>
2020-07-09 11:05:11 -0600

    datasetcreator

--- Dataset_Creator.py
+++ Dataset_Creator.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-from PIL import Image, ImageOps
+from PIL import Image
 
 images = []
 ages = []
@@ -9,17 +9,20 @@ genders = []
 def load_and_resize_images_from_folder(folder):
     for filename in os.listdir(folder):
         img = Image.open(os.path.join(folder,filename))
-        img = ImageOps.grayscale(img)
         img = img.resize((100, 100))
         imgarr = np.array(img)
-        images.append(imgarr)
-        extract_gender_and_age(filename)
+        #Filter out gray images
+        if imgarr.shape == (100, 100, 3):
+            images.append(imgarr)
+            extract_age_and_gender(filename)
         img.close()
 
-def extract_gender_and_age(filename):
+#Extract age and gender from the iamge's filename
+def extract_age_and_gender(filename):
 
     file_name_separator = filename.split('_')
 
+    #Save ages and genders into corresponding lists
     ages.append(int(file_name_separator[0]))
 
     genders.append(int(file_name_separator[1]))
