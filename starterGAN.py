b3b95531f4c6c26654ad197e3d727c6f01f67d3b
Logan Pepperl <loganp@Logans-MacBook-Pro-4.local>
2020-07-09 06:02:49 -0500

    added some actual code

--- starterGAN.py
+++ starterGAN.py
@@ -1,11 +1,8 @@
-'''from keras.layers import Input, Dense, Reshape, FLatten, Dropout, \
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout, \
     BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.layers.convolutional import Conv2D
 from keras.models import Sequential, Model
-from keras.optimizers import Adam
-import matplotlib.pyplot as plt
-import numpy as np'''
 
 #hyperparameters
 epochs = 0
@@ -15,24 +12,54 @@ save_interval = 0
 
 class GAN():
     def __init__(self):
-        #input shape
+        #input shape 
+        self.input_shape = (64, 64, 3) #64x64 RGB
         return
     
     def generator(self):
         #Input -> img
         #Output -> img + labels (age & gender)
+        
+        model = Sequential()
+        model.add(Conv2D(input_shape=self.input_shape))
         return
     
     def discriminator(self):
         #Input -> img + labels (age & gender)
         #Output -> real/fake classification
-        return
+        
+        model = Sequential()
+        model.add(Conv2D(filters=32, input_shape=self.input_shape))
+        model.add(LeakyReLU())
+        model.add(Dropout(0.25))
+        model.add(Conv2D(filters=62))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Flatten())
+        model.add(Dense(units=2))
+        
+        model.summary()
+        img = Input(shape=self.img_shape)
+        validity = model(img)
+
+        return Model(img, validity)         
     
-    def train(self):
-        #train discriminator to correctly classify generator      
-        #train generator to fool discriminator     
-        #repeat for num epochs
-        return
+    def train(self, e, bs, si):
+        for epoch in range(e):
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+            
+            #1. select random training set      
+            #2. generate new image/label pairings
+            #3. train discriminator on classified data
+            
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+            
+            #train generator
+            
+            return
     
 
 def getData():
@@ -40,4 +67,5 @@ def getData():
 
 
 if __name__ == '__main__':
-    True
+    g = GAN()
+    g.train(epochs, batch_size, save_interval)
