a0528baf26b484932c397be6a8ed3ad5238c1721
Logan Pepperl <loganp@Logans-MacBook-Pro-4.local>
2020-07-12 19:11:19 -0500

    save/load, small improvements

--- multioutputmodel.py
+++ multioutputmodel.py
@@ -1,12 +1,18 @@
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.layers import *
 from sklearn.model_selection import KFold
+from os.path import isfile
 
 class MultiOutputModel:
-    def __init__(self, rows=100, cols=100, channels=1):
-        self.img_shape = (rows, cols, channels)
-        return
-    
+    def __init__(self, mpath='model.model', load=False, rows, cols, channels=1):
+        self.img_shape = (rows, cols, channels)       
+        if isfile(mpath) and load:
+            print("Loading existing model")
+            self.m = load_model(mpath)
+        else:
+            print("Building new model")
+            self.build()
+
     def build(self):
         i = Input(shape=self.img_shape)
         x = Conv2D(32, kernal_size=3, strides=2, padding='same', input_shape=self.img_shape)(i)
@@ -22,18 +28,21 @@ class MultiOutputModel:
         x = LeakyReLU()(x)
         x = Dropout(.25)(x)
         x = Flatten()(x)
-        o1 = Dense(1, activation='sigmoid')(x)
-        o2 = Dense(1, activation=None)(x)
+        o1 = Dense(1, activation='sigmoid')(x) # gender
+        o2 = Dense(1, activation='relu')(x) # age
         
         self.m = Model(i, [o1, o2])
-        self.m.compile(optimizer='adam', loss=['binary_crossentropy', None], metrics='accuracy')
+        self.m.compile(optimizer='adam', loss=['binary_crossentropy', 'mean_squred_error'], metrics='accuracy')
     
     def train(self, X, y, e, bs):
         self.m.fit(X, [y[0], y[1]], epochs=e, batch_size=bs)
-        self.m.save
+        self.m.save()
     
     def test(self, X, y, bs):
         return self.m.evauluate(X, [y[0], y[1]], batch_size=bs, verbose=0)
+    
+    def predict(self, x):
+        return self.m.predict(x)
 
 def getData():
     return
@@ -42,7 +51,6 @@ def main():
     X, y = getData()
     
     M = MultiOutputModel()
-    M.build()
     kf = KFold(n_splits=10)
     i = 0
     for train_index, test_index in kf.split(X):
